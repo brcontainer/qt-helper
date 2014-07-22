af707f4ab0dcf0735fb4c6993e5406bd4d25e53d
Guilherme Nascimento <new_guil1503@hotmail.com>
2014-07-22 11:17:44 -0300

    Add LICENSE

--- application/runprocess.h
+++ application/runprocess.h
@@ -1,6 +1,13 @@
 #ifndef RUNPROCESS_H
 #define RUNPROCESS_H
 
+/*
+qt-helper 0.0.1
+Copyright (c) 2014 Guilherme Nascimento (brcontainer@yahoo.com.br)
+
+Released under the MIT license
+*/
+
 #include <QThread>
 #include <QString>
 
