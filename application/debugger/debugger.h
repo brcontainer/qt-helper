347207843f34856f2e1a90f5f951c421f584946a
Guilherme Nascimento <new_guil1503@hotmail.com>
2014-07-24 14:34:30 -0300

    Create debugger.h

---
+++ application/debugger/debugger.h
@@ -0,0 +1,44 @@
+#ifndef DEBUGGER_H
+#define DEBUGGER_H
+
+/*
+qt-helper 0.0.1
+Copyright (c) 2014 Guilherme Nascimento (brcontainer@yahoo.com.br)
+
+Released under the MIT license
+*/
+
+#include <QString>
+#include <QObject>
+#include <QTextStreamFunction>
+#include <QTextStreamManipulator>
+
+class debugger
+{
+public:
+    debugger(const bool active = true);
+    static void show(QChar t);
+    static void show(bool t);
+    static void show(char t);
+    static void show(signed short t);
+    static void show(unsigned short t);
+    static void show(signed int t);
+    static void show(unsigned int t);
+    static void show(signed long t);
+    static void show(unsigned long t);
+    static void show(qint64 t);
+    static void show(quint64 t);
+    static void show(float t);
+    static void show(double t);
+    static void show(const char* t);
+    static void show(const QString & t);
+    static void show(const QStringRef & t);
+    static void show(QLatin1String t);
+    static void show(const QByteArray & t);
+    static void show(const void * t);
+    static void show(QTextStreamFunction f);
+    static void show(QTextStreamManipulator m);
+    static void show(QObject *o);
+    static void show(QObject o);
+};
+#endif
